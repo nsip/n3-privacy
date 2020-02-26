252a187a7c824167a63082bffefb2828f43035a2
Qing Miao <cdutwhu@outlook.com>
2020-02-27 10:43:00 +1100

    * change funs name as json-util

--- Mask/main.go
+++ Mask/main.go
@@ -47,7 +47,7 @@ func doMask(inFilePath, maskFilePath, output string) {
 			}(i, json)
 		}
 		wg.Wait()
-		ioutil.WriteFile(output, []byte(jkv.MergeJSON(jsonList...)), 0666)
+		ioutil.WriteFile(output, []byte(jkv.MakeJSONArray(jsonList...)), 0666)
 
 	} else {
 		jkvD := jkv.NewJKV(data, "root", false)
