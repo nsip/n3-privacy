264105773a5c2bf5e4032617a87f2b6b7e11d5ba
Qing Miao <cdutwhu@outlook.com>
2020-03-11 13:07:10 +1100

    n3-privacy-Client [fullDump] argument

--- Client/v2.go
+++ Client/v2.go
@@ -16,59 +16,67 @@ import (
 func v2(cfgOK bool) {
 	cmn.FailOnErrWhen(!cfgOK, "%v", fEf("Config File Init Failed"))
 	cmn.FailOnErrWhen(len(os.Args) < 2, "%v", fEf("need subcommands: ["+sJoin(getCfgRouteFields(), " ")+"]"))
-	cmn.FailOnErrWhen(!initMapFnURL(glb.Cfg.Server.Protocol, glb.Cfg.Server.IP, glb.Cfg.WebService.Port), "%v", fEf("initMapFnURL fatal"))
-
-	timeout := time.After(time.Duration(glb.Cfg.Access.Timeout) * time.Second)
+	cmn.FailOnErrWhen(!initMapFnURL(glb.Cfg.Server.Protocol, glb.Cfg.Server.IP, glb.Cfg.WebService.Port), "%v", fEf("initMapFnURL Failed"))
+	arg1 := os.Args[1]
 	done := make(chan bool)
 
 	go func() {
 		var resp *http.Response = nil
 		var err error = nil
 		var data []byte = nil
-		url := mFnURL[os.Args[1]]
-		cmd := flag.NewFlagSet(os.Args[1], flag.ExitOnError)
+		url := mFnURL[arg1]
+		cmd := flag.NewFlagSet(arg1, flag.ExitOnError)
 		id := cmd.String("id", "", "policy ID")
-		user := cmd.String("user", "", "user")
-		ctx := cmd.String("ctx", "", "context")
-		object := cmd.String("object", "", "object")
+		user := cmd.String("u", "", "user")
+		ctx := cmd.String("c", "", "context")
+		object := cmd.String("o", "", "object")
 		rw := cmd.String("rw", "", "read/write")
-		policyPtr := cmd.String("policy", "", "the path of policy which is to be uploaded")
-		fullFlag := cmd.Bool("f", false, "output all attributes content from response")
+		policyPtr := cmd.String("p", "", "the path of policy which is to be uploaded")
+		fullDump := cmd.Bool("f", false, "output all attributes content from response")
 		cmd.Parse(os.Args[2:])
 
-		switch os.Args[1] {
+		mngMode := false
+
+		switch arg1 {
 		case "GetID":
-			cmn.FailOnErrWhen(*user == "", "%v", fEf("[-user] must be provided"))
-			cmn.FailOnErrWhen(*ctx == "", "%v", fEf("[-ctx] must be provided"))
-			cmn.FailOnErrWhen(*object == "", "%v", fEf("[-object] must be provided"))
-			cmn.FailOnErrWhen(*rw == "", "%v", fEf("[-rw] must be provided"))
+			cmn.FailOnErrWhen(*user == "", "%v", fEf("[-u] user must be provided"))
+			cmn.FailOnErrWhen(*ctx == "", "%v", fEf("[-c] context must be provided"))
+			cmn.FailOnErrWhen(*object == "", "%v", fEf("[-o] object must be provided"))
+			cmn.FailOnErrWhen(*rw == "", "%v", fEf("[-rw] read/write must be provided"))
 			url += fSf("?user=%s&ctx=%s&object=%s&rw=%s", *user, *ctx, *object, *rw)
 			resp, err = http.Get(url)
 
 		case "GetHash", "Get":
-			cmn.FailOnErrWhen(*id == "", "%v", fEf("[-id] must be provided"))
+			cmn.FailOnErrWhen(*id == "", "%v", fEf("[-id] ID must be provided"))
 			url += fSf("?id=%s", *id)
 			resp, err = http.Get(url)
 
 		case "Update":
-			cmn.FailOnErrWhen(*user == "", "%v", fEf("[-user] must be provided"))
-			cmn.FailOnErrWhen(*ctx == "", "%v", fEf("[-ctx] must be provided"))
-			cmn.FailOnErrWhen(*rw == "", "%v", fEf("[-rw] must be provided"))
+			cmn.FailOnErrWhen(*user == "", "%v", fEf("[-u] user must be provided"))
+			cmn.FailOnErrWhen(*ctx == "", "%v", fEf("[-c] context must be provided"))
+			cmn.FailOnErrWhen(*rw == "", "%v", fEf("[-rw] read/write must be provided"))
 			url += fSf("?user=%s&ctx=%s&rw=%s", *user, *ctx, *rw)
-			cmn.FailOnErrWhen(*policyPtr == "", "%v", fEf("[-policy] must be provided"))
+			cmn.FailOnErrWhen(*policyPtr == "", "%v", fEf("[-p] policy must be provided"))
 			policy, err := ioutil.ReadFile(*policyPtr)
-			cmn.FailOnErr("%v: %v", err, "Is [-policy] provided correctly?")
-			cmn.FailOnErrWhen(!cmn.IsJSON(string(policy)), "%v", fEf("policy is not valid JSON file, abort"))
+			cmn.FailOnErr("%v: %v", err, "Is [-p] policy provided correctly?")
+			cmn.FailOnErrWhen(!cmn.IsJSON(string(policy)), "%v", fEf("policy is invalid JSON file, abort"))
 			resp, err = http.Post(url, "application/json", bytes.NewBuffer(policy))
 
 		case "Delete":
-			cmn.FailOnErrWhen(*id == "", "%v", fEf("[-id] must be provided"))
+			cmn.FailOnErrWhen(*id == "", "%v", fEf("[-id] ID must be provided"))
 			url += fSf("?id=%s", *id)
 			req, err := http.NewRequest("DELETE", url, nil)
 			cmn.FailOnErr("%v", err)
 			resp, err = (&http.Client{}).Do(req)
 
+			// Management Functions:
 		case "ListID", "ListContext", "ListUser", "ListObject":
+			switch arg1 {
+			case "ListContext":
+				*ctx = ""
+			case "ListUser":
+				*user = ""
+			}
 			switch {
 			case *user != "" && *ctx != "":
 				url += fSf("?user=%s&ctx=%s", *user, *ctx)
@@ -78,9 +86,10 @@ func v2(cfgOK bool) {
 				url += fSf("?ctx=%s", *ctx)
 			}
 			resp, err = http.Get(url)
+			mngMode = true
 
 		default:
-			cmn.FailOnErr("%v", fEf("unknown subcommand: %v", os.Args[1]))
+			cmn.FailOnErr("%v", fEf("unknown subcommand: %v", arg1))
 		}
 
 		cmn.FailOnErr("%v", err)
@@ -89,28 +98,40 @@ func v2(cfgOK bool) {
 		data, err = ioutil.ReadAll(resp.Body)
 		cmn.FailOnErr("%v", err)
 
-		// fPln(string(data))
-		// Manage output TODO
+		const SepLn = "-----------------------------"
+
+		if *fullDump {
+			fPf("accessing... %s\n%s\n", url, SepLn)
+		}
 
 		if data != nil {
 			m := make(map[string]interface{})
 			cmn.FailOnErr("json.Unmarshal ... %v", json.Unmarshal(data, &m))
-			if *fullFlag {
-				fPln("accessing ... " + url)
-				fPln("-----------------------------")
-
-				if m["empty"] != nil && m["empty"] != "" {
-					fPf("Is Empty? %v\n", m["empty"])
+			if !mngMode {
+				if *fullDump {
+					if m["empty"] != nil && m["empty"] != "" {
+						fPf("Empty? %v\n%s\n", m["empty"], SepLn)
+					}
+					if m["error"] != nil && m["error"] != "" {
+						fPf("ERROR: %v\n%s\n", m["error"], SepLn)
+					}
 				}
-				fPln("-----------------------------")
-
-				if m["error"] != nil && m["error"] != "" {
-					fPf("ERROR: %v\n", m["error"])
+				if m["data"] != nil && m["data"] != "" {
+					fPf("%s\n", m["data"])
 				}
-				fPln("-----------------------------")
-			}
-			if m["data"] != nil && m["data"] != "" {
-				fPf("%s\n", m["data"])
+			} else {
+				key := ""
+				switch {
+				case *user != "" && *ctx != "":
+					key = fSf("%s@%s", *user, *ctx)
+				case *user != "":
+					key = *user
+				case *ctx != "":
+					key = *ctx
+				default:
+					key = "all"
+				}
+				fPf("%s\n", m[key])
 			}
 		}
 
@@ -118,8 +139,8 @@ func v2(cfgOK bool) {
 	}()
 
 	select {
-	case <-timeout:
-		cmn.FailOnErr("%v", fEf(fSf("Didn't Get Server Response in time. %d(s)", glb.Cfg.Access.Timeout)))
+	case <-time.After(time.Duration(glb.Cfg.Access.Timeout) * time.Second):
+		cmn.FailOnErr("%v", fEf(fSf("Didn't Get Server Response in %d(s)", glb.Cfg.Access.Timeout)))
 	case <-done:
 	}
 }
