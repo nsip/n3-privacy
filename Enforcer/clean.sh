c1b77137cc8571bb5f005546a77c6531572b8155
Qing Miao <cdutwhu@outlook.com>
2020-07-28 20:28:03 +1000

    * comment out Client wget

--- Enforcer/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf ./build
-rm -f *.log *.json
-rm -f ./process/*.log ./process/*.json
--- build.sh
+++
@@ -1,8 +0,0 @@
- #!/bin/bash
-
- set -e
- ORIGINALPATH=`pwd`
-
- cd Server && ./build.sh && cd $ORIGINALPATH && echo "Server built" 
- cd Enforcer && ./build.sh && cd $ORIGINALPATH && echo "Enforcer built"
- cd Client && ./build.sh && cd $ORIGINALPATH && echo "Client built"
